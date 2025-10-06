0487e3c3fc8f3486aed32501dd260a9a4c787a78
Fernando Walter <fernandowalter04@outlook.com>
2025-10-05 21:08:16 -0300

    Add files via upload

---
+++ JSONFuncao3.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import pyodbc
+
+app = Flask(__name__)
+
+# Configuração da conexão com o SQL Server
+def get_db_connection():
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=192.168.100.38,1433;'
+        'DATABASE=handsvii;'
+        'UID=consulta_python;'
+        'PWD=!Consulta1'
+    )
+    return conn
+
+@app.route('/funcao3', methods=['GET'])
+def get_funcao3():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM dbo.consulta_perctpimv('CASA')")
+    columns = [column[0] for column in cursor.description]
+    results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    conn.close()
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
